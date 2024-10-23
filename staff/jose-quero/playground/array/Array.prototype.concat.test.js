104630fe38779b5920e19f3f7a4d409e2551eff0
Jose Quero <jose_jarote1605@hotmail.com>
2024-10-23 17:36:15 +0200

    add array concat examples #6

---
+++ staff/jose-quero/playground/array/Array.prototype.concat.test.js
@@ -0,0 +1,12 @@
+console.log('TEST Array.prototype.push') // UNIR DOS ARRAYS EN UNO NUEVO
+
+console.log('CASE merge fruits and veggies')
+
+var fruits = ['apple', 'orange', 'lemon', 'banana', 'cherry']
+var vegetables = ['lettuce', 'onion', 'garlic', 'carrot', 'lentice']
+var food = fruits.concat(veggies)
+
+console.log(food)
+// ['apple', 'orange', 'lemon', 'banana', 'cherry', 'lettuce', 'onion', 'garlic', 'carrot', 'lentice']
+
+
