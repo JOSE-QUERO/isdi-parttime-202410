d828c48a00409bbb53f0bc94df10a64573351903
Jose Quero <jose_jarote1605@hotmail.com>
2024-10-24 19:32:04 +0200

    testing array examples #6

--- staff/jose-quero/playground/array/Array.prototype.concat.test.js
+++ staff/jose-quero/playground/array/Array.prototype.concat.test.js
@@ -9,4 +9,14 @@ var food = fruits.concat(veggies)
 console.log(food)
 // ['apple', 'orange', 'lemon', 'banana', 'cherry', 'lettuce', 'onion', 'garlic', 'carrot', 'lentice']
 
+console.log('CASE merge fruits and veggies and meats')
+
+var fruits = ['apple', 'orange', 'lemon', 'banana', 'cherry']
+var vegetables = ['lettuce', 'onion', 'garlic', 'carrot', 'lentice']
+var meats = ['cow', 'salmon', 'pig']
+var food = fruits.concat(veggies, meats)
+
+console.log(food)
+console.log(food)
+// ['apple', 'orange', 'lemon', 'banana', 'cherry', 'lettuce', 'onion', 'garlic', 'carrot', 'lentice', 'cow', 'salmon', 'pig']
 
