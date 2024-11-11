5764a6dc3639cc5a149860a4748ab987a52354c2
Jose Quero <jose_jarote1605@hotmail.com>
2024-11-11 12:51:14 +0100

    try new app from me #40

---
+++ staff/jose-quero/mi-app/app.js
@@ -0,0 +1,10 @@
+var button = document.getElementById('myButton');
+var output = document.getElementById('output');
+
+button.addEventListener('click', function () {
+    output.textContent = "¡ENHORABUENA CERDA!";
+});
+
+button.addEventListener('click', function () {
+    document.body.style.backgroundColor = "lightblue";
+});
